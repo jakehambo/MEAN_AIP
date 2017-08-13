7259cedb150caba5e5b6ade0fecd0d678a44569e
Jake <jake@Jakes-MacBook-Pro.local>
2017-08-13 16:53:54 +1000

    initial repo commit containing setup of stack and a quick demo

---
+++ server.js
@@ -0,0 +1,25 @@
+var express           = require('express'),
+    app               = express(),
+    bodyParser        = require('body-parser'),
+    mongoose          = require('mongoose'),
+    meetupsController = require('./server/controllers/meetups-controller');
+
+mongoose.connect('mongodb://localhost:27017/mean-demo');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/client/views/index.html');
+});
+
+app.use('/js', express.static(__dirname + '/client/js'));
+
+//REST API
+app.get('/api/meetups', meetupsController.list);
+app.post('/api/meetups', meetupsController.create);
+
+app.listen(3000, function() {
+  console.log('I\'m Listening...');
+})
