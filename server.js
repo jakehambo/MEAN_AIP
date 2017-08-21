bcf5cd655201d3ac976a63ee56fc5f06d8f8fbe3
Vinnie Araullo <Vicente.Araullo@student.uts.edu.au>
2017-08-21 16:16:34 +1000

    Amended server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 var express = require('express');
 var app = express();
 var mongojs = require('mongojs');
-var db = mongojs('moviesaip', ['moviesaip']);
+var db = mongojs('walksaip', ['walksaip']);
 var bodyParser = require('body-parser');
 
 //Showing where the views are
@@ -13,45 +13,45 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
 //REST METHOD function called get to call function in controller to display data
-app.get('/moviesaip', function (req, res) {
+app.get('/walksaip', function (req, res) {
   console.log('GET request');
-  db.moviesaip.find(function (err, docs) {
+  db.walksaip.find(function (err, docs) {
     console.log(docs);
     res.json(docs);
   });
 });
 
 //REST METHOD function called POST to add the data in the database
-app.post('/moviesaip', function (req, res) {
+app.post('/walksaip', function (req, res) {
   console.log(req.body);
-  db.moviesaip.insert(req.body, function(err, doc) {
+  db.walksaip.insert(req.body, function(err, doc) {
     res.json(doc);
   });
 });
 
 //REST METHOD function to delete the entry in the database
-app.delete('/moviesaip/:id', function (req, res) {
+app.delete('/walksaip/:id', function (req, res) {
   var id = req.params.id;
   console.log(id);
-  db.moviesaip.remove({_id: mongojs.ObjectId(id)}, function (err, doc) {
+  db.walksaip.remove({_id: mongojs.ObjectId(id)}, function (err, doc) {
     res.json(doc);
   });
 });
 
 //REST METHOD function to get a single entry of the data to be put in form
-app.get('/moviesaip/:id', function (req, res) {
+app.get('/walksaip/:id', function (req, res) {
   var id = req.params.id;
   console.log(id);
-  db.moviesaip.findOne({_id: mongojs.ObjectId(id)}, function (err, doc) {
+  db.walksaip.findOne({_id: mongojs.ObjectId(id)}, function (err, doc) {
     res.json(doc);
   });
 });
 
 //REST METHOD function to update a field in the database when user clicks edit
-app.put('/moviesaip/:id', function (req, res) {
+app.put('/walksaip/:id', function (req, res) {
   var id = req.params.id;
   console.log(req.body.title);
-  db.moviesaip.findAndModify({
+  db.walksaip.findAndModify({
     query: {_id: mongojs.ObjectId(id)},
     update: {$set: {title: req.body.title, releasedate: req.body.releasedate, duration: req.body.duration, genre: req.body.genre, synopsis: req.body.synopsis}},
     new: true}, function (err, doc) {
