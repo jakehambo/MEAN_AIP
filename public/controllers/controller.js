746421f6d55e8981b568f6fd2aad9e57fb18b89d
Vinnie Araullo <Vicente.Araullo@student.uts.edu.au>
2017-08-21 15:45:50 +1000

    Changed references from movie to walk

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -4,10 +4,10 @@ myBushWalkApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) 
 
 //Refresh the table
 var refresh = function() {
-  $http.get('/moviesaip').success(function(response) {
+  $http.get('/walksaip').success(function(response) {
     console.log("Refreshed");
-    $scope.moviesaip = response;
-    $scope.movie = "";
+    $scope.walksaip = response;
+    $scope.walk = "";
   });
 };
 
@@ -26,27 +26,27 @@ $scope.showAlert = function(ev) {
      $mdDialog.alert()
        .parent(angular.element(document.querySelector('#popupContainer')))
        .clickOutsideToClose(true)
-       .title('Updated Movie')
+       .title('Updated walk')
        .textContent('You can specify some description text in here.')
-       .ariaLabel('Movie Recommendation')
+       .ariaLabel('walk Recommendation')
        .ok('Okay')
        .targetEvent(ev)
    );
  };
 
-//Add the movie
-$scope.addMovie = function() {
-  console.log($scope.movie);
-  $http.post('/moviesaip', $scope.movie).success(function(response) {
+//Add the walk
+$scope.addwalk = function() {
+  console.log($scope.walk);
+  $http.post('/walksaip', $scope.walk).success(function(response) {
     console.log(response);
     refresh();
   });
 };
 
-//Remove the movie
+//Remove the walk
 $scope.remove = function(id) {
   console.log(id);
-  $http.delete('/moviesaip/' + id).success(function(response) {
+  $http.delete('/walksaip/' + id).success(function(response) {
     refresh();
   });
 };
@@ -54,22 +54,22 @@ $scope.remove = function(id) {
 //Get the info when clicked edit
 $scope.edit = function(id) {
   console.log(id);
-  $http.get('/moviesaip/' + id).success(function(response) {
-    $scope.movie = response;
+  $http.get('/walksaip/' + id).success(function(response) {
+    $scope.walk = response;
   });
 };
 
 //Update entries
 $scope.update = function() {
-  console.log($scope.movie._id);
-  $http.put('/moviesaip/' + $scope.movie._id, $scope.movie).success(function(response) {
+  console.log($scope.walk._id);
+  $http.put('/walksaip/' + $scope.walk._id, $scope.walk).success(function(response) {
     refresh();
   })
 };
 
 //Make empty fields
 $scope.deselect = function() {
-  $scope.movie = "";
+  $scope.walk = "";
 }
 
 }]);﻿
