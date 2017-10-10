f46b8d8132c74333463657cc863eec3f55fdc77f
Jake <jake@Jakes-MacBook-Pro.local>
2017-10-10 16:50:18 +1100

    added admin controller and added email verification module (need to implement)

---
+++ controllers/api/admins.controller.js
@@ -0,0 +1,40 @@
+﻿var config = require('config.json');
+var express = require('express');
+var router = express.Router();
+var adminService = require('services/admin.service');
+
+// routes
+router.post('/authenticate', authenticateUser);
+router.get('/current', getCurrentUser);
+
+module.exports = router;
+
+function authenticateUser(req, res) {
+    adminService.authenticate(req.body.username, req.body.password)
+        .then(function (token) {
+            if (token) {
+                // authentication successful
+                res.send({ token: token });
+            } else {
+                // authentication failed
+                res.status(401).send('Username or password is incorrect');
+            }
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+function getCurrentUser(req, res) {
+    adminService.getById(req.admin.sub)
+        .then(function (admin) {
+            if (admin) {
+                res.send(admin);
+            } else {
+                res.sendStatus(404);
+            }
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
